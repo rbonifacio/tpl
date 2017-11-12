d19842e031232f578d460bb70bd9f86570ffb4db
Leomar Camargo de Souza <leomarcamargodesouza@gmail.com>
2017-11-12 12:00:49 -0200

    Projection

--- 05.simpleExtensions/code/STLCExtensions.hs
+++ 05.simpleExtensions/code/STLCExtensions.hs
@@ -30,7 +30,7 @@ data Type = TBool
           | TUnit
           | TRecord [Maybe Type]
           | TTuple [Maybe Type]
-       -- | TProjection [Maybe Type]
+          | TProjection Type
           | TArrow Type Type
      deriving(Eq, Show)
 
@@ -44,10 +44,10 @@ data Term = Var Id
           | Unit
           | Record [RItem]
           | Tuple [TItem]
-       -- | Projection Label Term
+          | Projection Label Term
           | IfThenElse Term Term Term
           | Add Term Term
-        deriving(Eq, Show)  
+        deriving(Eq, Show) 
 
 data Value = VBool Bool
            | VInt Int
@@ -67,43 +67,45 @@ interp = undefined
 
 -- a definition of the type checker function for each expression.
 
-gamma |- (B b)              = Just TBool
+gamma |- (B b)                     = Just TBool
 
-gamma |- (N n)              = Just TInt
+gamma |- (N n)                     = Just TInt
 
-gamma |- (S s)              = Just TString
+gamma |- (S s)                     = Just TString
 
-gamma |- Unit               = Just TUnit
+gamma |- Unit                      = Just TUnit
 
-gamma |- (Var v)            = lookup v gamma >>= \t1 -> Just t1
+gamma |- (Var v)                   = lookup v gamma >>= \t1 -> Just t1
 
-gamma |- (Let v e1 e2)      = gamma          |- e1 >>= \t1 ->
-                              ((v,t1):gamma) |- e2 >>= \t2 ->
-                              Just t2                         
+gamma |- (Let v e1 e2)             = gamma          |- e1 >>= \t1 ->
+                                     ((v,t1):gamma) |- e2 >>= \t2 ->
+                                     Just t2                         
 
-gamma |- (Record items)     = let res = map (\(l,t) -> gamma |- t) items
-                              in Just (TRecord res) 
+gamma |- (Record items)            = let res = map (\(l,t) -> gamma |- t) items
+                                      in Just (TRecord res) 
 
-gamma |- (Tuple items)      = let res = map (\(t) -> gamma |- t) items
-                              in Just (TTuple res)
+gamma |- (Tuple items)             = let res = map (\(t) -> gamma |- t) items
+                                      in Just (TTuple res)
 
-gamma |- (Lambda (x, t1) t) = ((x,t1):gamma) |- t >>= \t2 -> return (TArrow t1 t2)
+gamma |- (Projection label record) = gamma |- (search (label) (record)) >>= \t1 -> return (TProjection t1)
 
-gamma |- (App e1 e2)        = gamma |- e1 >>= \t1 ->
-                              gamma |- e2 >>= \t2 ->
-                              case t1 of
-                                (TArrow t11 t12) -> if t12 == t2 then Just t12 else Nothing
-                                otherwise -> Nothing
+gamma |- (Lambda (x, t1) t)        = ((x,t1):gamma) |- t >>= \t2 -> return (TArrow t1 t2)
+
+gamma |- (App e1 e2)               = gamma |- e1 >>= \t1 ->
+                                     gamma |- e2 >>= \t2 ->
+                                     case t1 of
+                                     (TArrow t11 t12) -> if t12 == t2 then Just t12 else Nothing
+                                     otherwise -> Nothing
                          
-gamma |- (IfThenElse c t e) = gamma |- c >>= \t1 ->
-                              gamma |- t >>= \t2 ->
-                              gamma |- e >>= \t3 ->
-                              if(t1 == TBool && t2 == t3) then (Just t2) else Nothing
+gamma |- (IfThenElse c t e)        = gamma |- c >>= \t1 ->
+                                     gamma |- t >>= \t2 ->
+                                     gamma |- e >>= \t3 ->
+                                     if(t1 == TBool && t2 == t3) then (Just t2) else Nothing
 
     
-gamma |- (Add e1 e2)        = gamma |- e1 >>= \t1 ->
-                              gamma |- e2 >>= \t2 ->
-                              if(t1 == TInt && t2 == TInt) then return TInt else Nothing
+gamma |- (Add e1 e2)               = gamma |- e1 >>= \t1 ->
+                                     gamma |- e2 >>= \t2 ->
+                                     if(t1 == TInt && t2 == TInt) then return TInt else Nothing
 
 
 -- | A lookup function. It searches for a specific
@@ -121,9 +123,15 @@ match :: [Label] -> [Term] -> Maybe Term
 match ls ts = Record <$> matchItems ls ts 
 
 
--- | A match items funciton. It combines the labels and terms
+-- | A match items function. It combines the labels and terms
 -- in a list so that the Record is built
 matchItems :: [l] -> [t] -> Maybe [(l,t)]
-matchItems [] [] = Just []
+matchItems [] []         = Just []
 matchItems (l:ls) (t:ts) = ((l,t):) <$> matchItems ls ts
-matchItems _ _ = Nothing
+matchItems _ _           = Nothing
+
+-- | A search function to records. It looks for a certain element in
+-- the record by its label and returns its value
+search :: Label -> Term -> Term
+search _ (Record [])                  = error "element in Record not found"
+search (x) (Record ((label,item):xs)) = if x == label then item else search (x) (Record xs)

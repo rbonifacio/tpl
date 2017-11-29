fe0bc78bafc31935d25a799e40e680054a025f1f
Diego Marcilio <dvmarcilio@gmail.com>
2017-11-29 20:59:58 -0200

    Adding more test cases, specially for TArrow.
    - Changing all assertions to assertEqual
    -- Output is clearer. (shows expected value)

--- 05.simpleExtensions/code/SubTypeTests.hs
+++ 05.simpleExtensions/code/SubTypeTests.hs
@@ -6,6 +6,7 @@ import Test.HUnit
 -- run the test suite: runTestTT testSuite
 
 testSuite = TestList [ 
+    TestLabel "Record: r2 <: r" recc,
     TestLabel "Record: r1 <: r3" rec1,
     TestLabel "Record: r1 <: r2" rec2,
     TestLabel "Record: RcdWidth - r2 <: r1" rec3,
@@ -13,9 +14,17 @@ testSuite = TestList [
     TestLabel "Record: RcdPerm - r2 <: r2perm" rec5,
     TestLabel "Record: RcdPerm - r2perm <: r2" rec6,
     TestLabel "Record: RcdDepth - r4 <: r5" rec7,
-    TestLabel "Record: RcdDepth - r5 <: r4" rec8
+    TestLabel "Record: RcdDepth - r5 <: r4" rec8,
+    TestLabel "TBool <: TBool" reflTBool,
+    TestLabel "TInt <: TInt" reflTInt, 
+    TestLabel "TString <: TString" reflTString,
+    TestLabel "arr <: arr" reflArr,
+    TestLabel "Arrow: arr1 <: arr" arrow1,
+    TestLabel "Arrow: arr2 <: arr" arrow2,
+    TestLabel "Arrow: arr3 <: arr " arrow3
   ]
 
+r = TRecord [("y", TString)]
 r1 = TRecord [("x", TInt)]
 r2 = TRecord [("x", TInt), ("y", TString)]
 r2perm = TRecord [("y", TString), ("x", TInt)]
@@ -24,19 +33,40 @@ innerRecX = TRecord [("a", TInt), ("b", TInt)]
 innerRecY = TRecord [("m", TInt), ("n", TInt)]
 r4 = TRecord [("x", innerRecX), ("y", innerRecY)]
 r5 = TRecord [("x", TRecord [("a", TInt)]), ("y", TRecord [("n", TInt)]) ]
+
+-- T3 <: T1      T2 <: T4
+-- ----------------------
+-- T1 -> T2  <:  T3 -> T4
+arr  = TArrow  (TRecord [("x", TInt), ("y", TInt), ("z", TInt)]) (TRecord [("x", TInt), ("y", TInt)])
+arr1 = TArrow (TRecord [("x", TInt), ("y", TInt)]) (TRecord [("x", TInt), ("y", TInt), ("w", TBool)])
+arr2 = TArrow (TRecord [("y", TInt)]) (TRecord [("w", TBool)])
+arr3 = TArrow (TRecord [("w", TInt)]) (TRecord [("x", TInt), ("y", TInt)])
+
+-- Test cases
+
+recc = TestCase $ assertEqual "{x: Int, y: String} should be subtype of {y: String}" True (r2 <: r)
   
-rec1 = TestCase $ assertBool "TInt should be subtype of TTop" $ r1 <: r3
+rec1 = TestCase $ assertEqual "{x: Top} should not be subtype of {x: Int}" True (r1 <: r3)
 
-rec2 = TestCase $ assertEqual "TTop should not be subtype of TInt" (r3 <: r1) False
+rec2 = TestCase $ assertEqual "{x: Int} should be subtype of {x: Top}" False (r3 <: r1)
 
-rec3 = TestCase $ assertBool "Record with at least all fields of super type should be a subtype" $ r2 <: r3
+rec3 = TestCase $ assertEqual "Record with at least all fields of super type should be a subtype" True (r2 <: r3)
 
-rec4 = TestCase $ assertEqual "Record that doesn't have all fields of super type should not be a subtype" (r1 <: r2) False
+rec4 = TestCase $ assertEqual "Record that doesn't have all fields of super type should not be a subtype" False (r1 <: r2)
 
-rec5 = TestCase $ assertBool "Order of fields should not matter in a subtype relation for records" $ r2 <: r2perm
+rec5 = TestCase $ assertEqual "Order of fields should not matter in a subtype relation for records" True (r2 <: r2perm)
 
-rec6 = TestCase $ assertBool "Order of fields should not matter in a subtype relation for records" $ r2perm <: r2
+rec6 = TestCase $ assertEqual "Order of fields should not matter in a subtype relation for records" True (r2perm <: r2)
 
-rec7 = TestCase $ assertBool "Corresponding fields should keep a subtype relation" $ r4 <: r5
+rec7 = TestCase $ assertEqual "Corresponding fields should keep a subtype relation" True (r4 <: r5)
 
-rec8 = TestCase $ assertEqual "Corresponding fields should keep a subtype relation" (r5 <: r4) False
+rec8 = TestCase $ assertEqual "Corresponding fields should keep a subtype relation" False (r5 <: r4)
+
+reflTInt = TestCase $ assertEqual "TInt should be subtype of itself" True (TInt <: TInt)
+reflTBool = TestCase $ assertEqual "TInt should be subtype of itself" True (TBool <: TBool)
+reflTString = TestCase $ assertEqual "TInt should be subtype of itself" True (TString <: TString)
+
+reflArr = TestCase $ assertEqual "arr should be subtype of itself" True (arr <: arr)
+arrow1 = TestCase $ assertEqual "Should be subtype" True (arr1 <: arr)
+arrow2 = TestCase $ assertEqual "Should not be subtype" False (arr2 <: arr)
+arrow3 = TestCase $ assertEqual "Should not be subtype" False (arr3 <: arr)
